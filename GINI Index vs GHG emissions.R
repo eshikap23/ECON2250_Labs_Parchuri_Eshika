45b7f8ee248924246b686cf6e5741d804703e71f
Eshika Parchuri <Eshika.Parchuri@gmail.com>
2025-11-30 01:11:31 -0500

    scatter plot

--- GINI Index vs GHG emissions.R
+++ GINI Index vs GHG emissions.R
@@ -9,9 +9,16 @@ countries <- read_csv("StatsforEconFinalData2.csv")
 ghg_reg <- lm(countries$GHG_Emission_Per_Capita ~ countries$GINI_Index)
 summary(ghg_reg)
 
+#scatter plot for linear regression
+plot(countries$GINI_Index, countries$GHG_Emission_Per_Capita, col = "black",pch=21, bg = "lightblue",
+     main = "Greenhouse Gas Emissions Per Capita vs. GINI index",
+     xlab = "GINI Index", ylab = "Greenhouse Gas Emissions Per Capita")
+#trendline for scatter plot
+abline(ghg_reg, col = "red",lwd=2)
+
 #Right-tailed t-test using correlation test 
-#countries$GINI_Index <- gsub("\u00A0", "", countries$GINI_Index)
-countries$GINI_Index <- as.numeric(countries$GINI_Index)
+
+countries$GINI_Index <- as.numeric(countries$GINI_Index) #made sure all values were numbers
 
 
 ghg_test <- cor.test(countries$GINI_Index, countries$GHG_Emission_Per_Capita,
